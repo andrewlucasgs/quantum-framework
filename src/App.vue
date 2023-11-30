b8addcdac499afea4e7b445ce2a4837478cad1d2
Andrew Lucas <andrewlucasgs@gmail.com>
2023-11-30 12:02:07 -0500

    Improving UI

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,9 @@ import { evaluate } from 'mathjs'
 const classicalRuntime = ref("n");
 const quantumRuntime = ref("\\sqrt{n}");
 
-const logicalQubits = ref("");
+const logicalQubits = ref("1000");
 const hardwareSlowdown = ref(6);
-const projectedQubits = ref("");
+const projectedQubits = ref("IBM");
 
 const hardwares = ref([]);
 
@@ -24,7 +24,7 @@ const problems = ref([
   {
     name: "N squared",
     classicalRuntime: "n^2",
-    quantumRuntime: "n*log(n)",
+    quantumRuntime: "n*log10(n)",
   },
   {
     name: "Exponential",
@@ -59,7 +59,7 @@ const quantumPoints = computed(() => {
     quantumIsFaster.value * 1,
 
     quantumIsFaster.value * quantumIsFaster.value,
-    ]
+  ]
 
   for (let i of nValues) {
     points.push({ y: quantumFunction(i), x: i });
@@ -90,7 +90,8 @@ const classicalPoints = computed(() => {
   return [...points.map((point) => ({
     y: point.y === 0 ? 0 : Math.log10(point.y),
     x: point.x === 0 ? 0 : Math.log10(point.x)
-  }))]})
+  }))]
+})
 
 
 function findN(f, g, start = 0, end = 10 ** 300, tolerance = 0.1) {
@@ -150,10 +151,10 @@ const chartOptions = ref({
     plotLines: [{
       color: '#FF0000', // Red
       width: 2,
-      value: Math.round(quantumIsFaster.value === 0 ? 0 : Math.log10(quantumIsFaster.value)),
+      value: quantumIsFaster.value < 1 ? Math.round(quantumIsFaster.value === 0 ? 0 : (Math.log10(quantumIsFaster.value) * 10)) / 10 : Math.round(quantumIsFaster.value === 0 ? 0 : (Math.log10(quantumIsFaster.value) * 100)) / 100,
       label: {
         useHTML: true,
-        text: `N* = 10<sup>${Math.log10(quantumIsFaster.value).toFixed(2)}</sup>`,
+        text: quantumIsFaster.value < 100 ? `N* = ${quantumIsFaster.value}` : `N* = 10<sup>${quantumIsFaster.value < 1 ? Math.round(quantumIsFaster.value === 0 ? 0 : (Math.log10(quantumIsFaster.value) * 10)) / 10 : Math.round(quantumIsFaster.value === 0 ? 0 : (Math.log10(quantumIsFaster.value) * 100)) / 100}</sup>`,
         rotation: 0,
         verticalAlign: 'bottom',
         style: {
@@ -169,6 +170,9 @@ const chartOptions = ref({
     labels: {
       useHTML: true,
       formatter: function () {
+        if (quantumIsFaster.value < 100) {
+          return `<p>${(10**this.value).toFixed(0)}</p>`
+        }
         return `10<sup>${this.value}</sup>`
       }
     },
@@ -190,15 +194,28 @@ const chartOptions = ref({
       labels: {
         useHTML: true,
         formatter: function () {
+
           return `10<sup>${this.value}</sup>`
         }
       },
     },
-   ],
+  ],
   series: [{
     name: 'Quantum',
     yAxis: 0,
     type: 'spline',
+    color: '#0000FF',
+    dataLabels: {
+      enabled: true,
+      useHTML: true,
+      formatter: function () {
+        // if last point
+        if (this.point === this.series.data[this.series.data.length - 1]) {
+          return `<span style="color: #0000FF; font-weight: bold;">${this.series.name}</span>`
+        }
+      }
+    },
+
     // HIDE POINTS
     marker: {
       enabled: false
@@ -210,6 +227,17 @@ const chartOptions = ref({
     name: 'Classical',
     data: classicalPoints.value,
     type: 'spline',
+    color: "#006400",
+    dataLabels: {
+      enabled: true,
+      useHTML: true,
+      formatter: function () {
+        // if last point
+        if (this.point === this.series.data[this.series.data.length - 1]) {
+          return `<span style="color: #006400; font-weight: bold;">${this.series.name}</span>`
+        }
+      }
+    },
     marker: {
       enabled: false
     },
@@ -222,8 +250,8 @@ const key = ref(0);
 watch([quantumRuntime, classicalRuntime, hardwareSlowdown], () => {
   chartOptions.value.series[0].data = quantumPoints.value;
   chartOptions.value.series[1].data = classicalPoints.value;
-  chartOptions.value.xAxis.plotLines[0].value = Math.round(quantumIsFaster.value === 0 ? 0 : (Math.log10(quantumIsFaster.value) * 100)) /100
-  chartOptions.value.xAxis.plotLines[0].label.text = `N* = 10<sup>${Math.log10(quantumIsFaster.value).toFixed(2)}</sup>`
+  chartOptions.value.xAxis.plotLines[0].value = quantumIsFaster.value < 1 ? Math.round(quantumIsFaster.value === 0 ? 0 : (Math.log10(quantumIsFaster.value) * 10)) / 10 : Math.round(quantumIsFaster.value === 0 ? 0 : (Math.log10(quantumIsFaster.value) * 100)) / 100
+  chartOptions.value.xAxis.plotLines[0].label.text = quantumIsFaster.value < 100 ? `N* = ${quantumIsFaster.value.toFixed(2)}` : `N* = 10<sup>${quantumIsFaster.value < 1 ? Math.round(quantumIsFaster.value === 0 ? 0 : (Math.log10(quantumIsFaster.value) * 10)) / 10 : Math.round(quantumIsFaster.value === 0 ? 0 : (Math.log10(quantumIsFaster.value) * 100)) / 100}</sup>`
   key.value += 1;
 
   console.log(quantumIsFaster.value)
@@ -235,47 +263,54 @@ watch([quantumRuntime, classicalRuntime, hardwareSlowdown], () => {
 </script>
 
 <template>
-  <div>
-    <div class="bg-slate-100 flex gap-8 p-8">
-      <form class="w-64 flex flex-col gap-2 items-start">
-        <h2 class="text-2xl font-bold text-slate-900">Problem</h2>
-        <label for="problem"></label>
-        <select class="border-2 border-gray-500 rounded-md p-2" name="problem" id="problem" v-model="selectedProblem">
-          <option v-for="problem in problems" :key="problem.name" :value="problem">{{ problem.name }}</option>
-        </select>
-        <label class=" font-bold text-slate-900" for="classicalRuntime">Classical Runtime:
-        </label>
-        <input class="border-2 border-gray-500 rounded-md p-2" type="text" id="classicalRuntime" name="classicalRuntime"
-          v-model="classicalRuntime">
-        <label class=" font-bold text-slate-900" for="quantumRuntime">Quantum Runtime:
-        </label>
-        <input class="border-2 border-gray-500 rounded-md p-2" type="text" id="quantumRuntime" name="quantumRuntime"
-          v-model="quantumRuntime">
-
-        <!-- <input class="border-2 border-gray-500 rounded-md p-2" type="submit" value="Add Problem"> -->
-      </form>
-      <form class="w-64 flex flex-col gap-2 items-start">
-        <h2 class="text-2xl font-bold text-slate-900">Hardware</h2>
-        <label class=" font-bold text-slate-900" for="logicalQubits">Logical Qubits:
-        </label>
-        <input class="border-2 border-gray-500 rounded-md p-2" type="text" id="logicalQubits" name="logicalQubits"
-          v-model="logicalQubits">
-        <label class=" font-bold text-slate-900" for="hardwareSlowdown">Hardware Slowdown: 10 <sup>{{ hardwareSlowdown
-        }}</sup>
-        </label>
-        <input id="hardwareSlowdown" v-model="hardwareSlowdown" type="range" min="0" max="100" step="1"
-          class="w-full h-1 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-white border border-black">
-
-        <label class=" font-bold text-slate-900" for="projectedQubits">Projected Qubits:
-        </label>
-        <input class="border-2 border-gray-500 rounded-md p-2" type="text" id="projectedQubits" name="projectedQubits"
-          v-model="projectedQubits">
-
-        <!-- <input class="border-2 border-gray-500 rounded-md p-2" type="submit" value="Add Hardware"> -->
-      </form>
+  <div class="">
+    <div class="p-4">
+      <h1 class="text-4xl font-bold text-center uppercase">Quantum Advantage Calculator</h1>
     </div>
+    <div class="bg-slate-100 flex gap-8 p-8">
+      <div class="max-w-7xl mx-auto grid grid-cols-2 gap-8">
+        <form class="w-64 flex flex-col gap-2 items-start">
+          <h2 class="text-2xl font-bold text-slate-900">Problem</h2>
+          <label for="problem" class=" font-bold text-slate-900">Problem</label>
+          <select class="border-2 border-gray-500 bg-white rounded-md p-2 w-full" name="problem" id="problem"
+            v-model="selectedProblem">
+            <option v-for="problem in problems" :key="problem.name" :value="problem">{{ problem.name }}</option>
+          </select>
+          <label class=" font-bold text-slate-900" for="classicalRuntime">Classical Runtime:
+          </label>
+          <input class="border-2 border-gray-500 rounded-md p-2" type="text" id="classicalRuntime" name="classicalRuntime"
+            v-model="classicalRuntime">
+          <label class=" font-bold text-slate-900" for="quantumRuntime">Quantum Runtime:
+          </label>
+          <input class="border-2 border-gray-500 rounded-md p-2" type="text" id="quantumRuntime" name="quantumRuntime"
+            v-model="quantumRuntime">
 
-    <Chart :key="key" :options="chartOptions" />
+        </form>
+
+        <form class="w-64 flex flex-col gap-2 items-start">
+          <h2 class="text-2xl font-bold text-slate-900">Hardware</h2>
+          <label class=" font-bold text-slate-900" for="logicalQubits">Logical Qubits:
+          </label>
+          <input class="border-2 border-gray-500 rounded-md p-2" type="text" id="logicalQubits" name="logicalQubits"
+            v-model="logicalQubits">
+          <label class=" font-bold text-slate-900" for="hardwareSlowdown">Hardware Slowdown: 10 <sup>{{ hardwareSlowdown
+          }}</sup>
+          </label>
+          <input id="hardwareSlowdown" v-model="hardwareSlowdown" type="range" min="0" max="50" step="1"
+            class="my-4 w-full h-2 p-1 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-white border border-black">
+
+          <label class=" font-bold text-slate-900" for="projectedQubits">Projected Qubits:
+          </label>
+          <input class="border-2 border-gray-500 rounded-md p-2" type="text" id="projectedQubits" name="projectedQubits"
+            v-model="projectedQubits">
+
+        </form>
+      </div>
+    </div>
+    <div class="max-w-7xl mx-auto mt-4">
+
+      <Chart :key="key" :options="chartOptions" />
+    </div>
 
   </div>
 </template>
